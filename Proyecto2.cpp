9b6ba6cdcb26a4694896728cd341b5a62863127b
Rebecca Smith <70491756+rsmith1220@users.noreply.github.com>
2021-09-21 16:03:54 -0600

    se agrego el menu y la opciones para el usuario

--- Proyecto2.cpp
+++ Proyecto2.cpp
@@ -23,9 +23,64 @@
 
 #include "BFS.cpp" // Llamar elementos del archivo "BFS.cpp"
 
+int menu()
+{
+    int opcion;
+
+    printf("Elija una de las siguientes opciones\n");
+    printf("\n1. Buscar un numero en el arbol");
+    printf("\n2. Desplegar el arbol");
+    printf("\n3. Agregar un numero al arbol");
+    printf("\n4. Salir del programa\n");
+
+    opcion = 0;
+    
+    scanf("%d",opcion);
+
+    int num;
+    int agregar;
+
+    switch (opcion)
+    {
+    case 1:
+    //se busca un numero en el arbol
+        printf("\nIngrese el numero que desea buscar");
+        num =0;
+        scanf("%d",num);
+        break;
+
+    case 2:
+        //se imprime el arbol asi re nice
+        break;
+
+    case 3:
+        //se le agrega un numero al arbol
+        printf("\nIngrese el numero que desea agregar");
+        agregar = 0;
+        scanf("%d",agregar);
+        break;
+
+    case 4:
+        printf("\nAdios :)");
+        break;
+
+
+    default:
+
+        printf("\nIngrese una opcion valida");
+        break;
+    }
+
+    return 0; 
+
+}
+
+
 int main(void)
 {
     printf("----- PROYECTO #2 - Microprocesadores -----\n");
+
+    printf("%d",menu());
 
     Graph grafo(2);
 
