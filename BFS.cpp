e91ca9068a51c4b204f8a70785b998565c87f851
CRLB-sketch <cristianlaynezbachez@gmail.com>
2021-10-01 09:35:21 -0600

    BFS pequenio avance

--- BFS.cpp
+++ BFS.cpp
@@ -10,12 +10,41 @@
 using namespace std;
 #define NTHREADS 17
 
+
 struct Check
 {
 	int i;
 	list<int> queue;
 	bool *visited ;
+	pthread_mutex_t lock;
 };
+
+struct Check testG;
+
+void* checkAdj(void *arg)
+{
+	struct Check *checking;
+	checking = (struct Check *)arg;
+	pthread_mutex_t lock = checking -> lock;
+
+	//pthread_mutex_lock(&lock);
+
+	int i = checking -> i;
+	int* ptr_i = &i;
+	list<int> queue = checking -> queue;
+	bool visited = checking -> visited;
+
+	if(!visited[ptr_i]){
+		visited[ptr_i] = true;
+			
+		queue.push_back(*ptr_i);
+	}
+
+	//pthread_mutex_unlock(&lock);		
+	testG.queue = queue;	
+
+	return (void*)1;
+}
 
 class Graph{
 private:
@@ -25,7 +54,7 @@ private:
 	//puntero lista de adyacencia
 	list<int>* adj;
 
-	// Preprar cancelar
+	// Preprar cancelar	
 	pthread_mutex_t lock;
 		
 public:
@@ -45,10 +74,12 @@ public:
 		adj[v].push_back(v2);
 	}
 
+	
+
 	//metodo de fuente del grafo
 	string bfs(int s)
 	{
-		string information = "";
+		string information = "XD:";
 		//ningun vertice a sido recorrido
 		bool *visited = new bool[vert];
 		for(int i = 0; i < vert; i++){
@@ -70,12 +101,19 @@ public:
 		void *returned;
 
 		// Para llevar a cabo la ejecución de hilos
+		pthread_t tid;
 		int createTreads;		
+
+		if(pthread_mutex_init(&lock, NULL) != 0)
+		{
+			return "ERROR: La inicializacion de mutex fallo";
+		}
 
 		//sigue hasta que la cola este vacia
 		while(!queue.empty()){
 			s = queue.front();
 			information += to_string(s) + " ";
+			cout << s << endl;
 			queue.pop_front();
 		
 			// cout<< "Revisando los vertices adyacentes"<< s << endl;
@@ -83,16 +121,18 @@ public:
 			for(auto i = adj[s].begin(); i != adj[s].end(); i++){
 			//solo los nodos que no han sido recorridos
 				check.i = *i;
+				
+				createTreads = pthread_create(&tid, NULL, checkAdj, (void *)&check);
+				
+				pthread_join(tid, &returned);
 
-
-				if(!visited[*i]){
-					//cout<<"nodo visitado"<< *i << endl;
-					visited[*i] = true;
-			
-					queue.push_back(*i);
-				}
+				//queue = (Check)returned;
+				queue = testG.queue;
 			}		
 		}	
+
+		pthread_mutex_destroy(&lock);
+
 		return information;
 	}
 };
